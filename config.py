14d40ed9e21ec8dd765025d8bc501085290fc236
shruthi <shruthi.nagendra@utdallas.edu>
2021-10-31 22:28:51 -0500

    commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 dbparams = {
-    'host': '',
-    'username': '',
-    'password': '',
-    'database': ''
+    'host': 'besttrade.cp9zmx3khkea.us-east-1.rds.amazonaws.com',
+    'username': 'admin',
+    'password': 'password',
+    'database': 'besttrade'
 }
