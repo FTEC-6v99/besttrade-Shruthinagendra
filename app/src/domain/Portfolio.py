1eb8ddafd937fdbd59f4570567c3918e409b99dd
Shruthinagendra <89654710+Shruthinagendra@users.noreply.github.com>
2021-12-19 20:19:26 -0600

    Delete app/src directory

--- app/src/domain/Portfolio.py
+++
@@ -1,6 +0,0 @@
-class Portfolio():
-    def __init__(self, ticker=str, quantity=int, purchase_price=float, account_number: int = -1):
-        self.account_number = account_number
-        self.ticker = ticker
-        self.quantity = quantity
-        self.purchase_price = purchase_price
